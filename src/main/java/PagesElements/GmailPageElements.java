ab0c96fb673dfbbb6c49c9af87072a239e531415
iamdanone <dennismikheye@gmail.com>
2021-10-21 20:59:04 +0300

    A to B test is finished

---
+++ src/main/java/PagesElements/GmailPageElements.java
@@ -0,0 +1,16 @@
+package PagesElements;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class GmailPageElements {
+    public SelenideElement userNameField = $x("(//*[contains(@id, 'identifierId')])");
+    public SelenideElement nextButton = $x("(//*[contains(text(), 'Далее')])");
+    public SelenideElement userPasswordField = $x("(//*[contains(@autocomplete, 'current-password')])");
+    public SelenideElement inboxButton = $x("(//*[contains(@class, 'nU n1')])");
+    public SelenideElement gmailButton = $x("(//*[contains(text(), 'Gmail')])");
+    public SelenideElement unreadMessage = $x("(//*[contains(@class, 'zA zE')])");
+    public SelenideElement invitationCode = $x("(//*[contains(@data-saferedirecturl, " +
+            "'https://www.google.com/url?q=https://dev.marketcapp.ai/accept-invite?invitation-code')])");
+}
